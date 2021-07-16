a3fb8a4e23b8c8adbdfcb6fb1f598d2561bb274c
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2021-07-16 17:13:48 -0500

    Se agrega un marcador al mapa.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- src/app/components/map/component/map.component.ts
+++ src/app/components/map/component/map.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit, AfterViewInit, Input } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Input, } from '@angular/core';
 
-import { Map, tileLayer } from 'leaflet';
+import { Map, tileLayer, Icon, icon, Marker } from 'leaflet';
 
 import { MapService } from '../services/map.service';
 
@@ -12,6 +12,8 @@ import { MapService } from '../services/map.service';
 export class MapComponent implements OnInit, AfterViewInit {
 
   private map!: Map;
+  private icon!: Icon;
+  private marker!: Marker;
 
   @Input() public lat!: number;
   @Input() public lng!: number;
@@ -32,9 +34,49 @@ export class MapComponent implements OnInit, AfterViewInit {
 
   private initMap(): void {
     //Inicializa al mapa.
-    this.map = new Map('map').setView([this.lat, this.lng], this.zoom);
-    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { //Carga la capa base para el mapa.
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors',
-    }).addTo(this.map);
+    this.map = new Map('map')
+      .setView([this.lat, this.lng], this.zoom);
+
+    tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+      { //Carga la capa base para el mapa.
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors',
+      }
+    ).addTo(this.map);
+
+    // Crea un nuevo icono personalizado para el marcador.
+    this.icon = icon({
+      iconUrl: 'assets/markers/marker.png',
+      iconSize: [50, 90],
+      iconAnchor: [22, 94],
+      popupAnchor: [2, -70]
+    });
+
+    this.marker = new Marker([this.lat, this.lng], { icon: this.icon });
+    this.marker.addTo(this.map);
+
+    this.map.on('click', this.changeMakerLatLng);
+    this.marker.on('move', this.getMarkerLatLng);
+  }
+
+  /**
+   * Cambia la latitud y longitud del marcador en el mapa.
+   * 
+   * @param event 
+   */
+  private changeMakerLatLng = (event: any) => {
+    const latlng = event.latlng;
+    this.marker.setLatLng(latlng);
+    this.marker.addTo(this.map);
+  }
+
+  /**
+  * Obtiene la latitud y longitud de la posición actual del marcador en el mapa.
+  * 
+  * @param event 
+  */
+  private getMarkerLatLng = (event: any) => {
+    const latlng = event.latlng;
+    this.lat = latlng.lat;
+    this.lng = latlng.lng;
   }
 }
